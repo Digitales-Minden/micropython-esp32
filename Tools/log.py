6d1a6147944d174eba7c6a8e7362193da0b04bad
McUtty <donmcutty@gmail.com>
2019-04-24 23:13:00 +0200

    Sammlung von nützlichen Tools

---
+++ Tools/log.py
@@ -0,0 +1,24 @@
+import os
+
+class Log:
+
+    def __init__(self, logname):
+        self.filename = str(logname + ".txt")
+
+
+    def logwrite(self, logtxt):
+        file = open (self.filename, "a")
+        file.write(logtxt + " \n")
+        file.close()
+
+
+    def readaline(self):
+        file = open(self.filename, "r")
+        print(file.readline())
+        file.close()
+
+
+    def readfile(self):
+        file = open(self.filename, "r")
+        print(file.read())
+        file.close()
