5013d7413cb0016235d13446f2d8bd52b0e1ffdb
McUtty <donmcutty@gmail.com>
2019-04-24 23:18:43 +0200

    Ein LogFile definieren, schreiben und lesen

--- Tools/log.py
+++ Tools/log.py
@@ -3,20 +3,17 @@ import os
 class Log:
 
     def __init__(self, logname):
-        self.filename = str(logname + ".txt")
-
+        self.filename = str(logname + ".log")
 
     def logwrite(self, logtxt):
         file = open (self.filename, "a")
         file.write(logtxt + " \n")
         file.close()
 
-
     def readaline(self):
         file = open(self.filename, "r")
         print(file.readline())
         file.close()
-
 
     def readfile(self):
         file = open(self.filename, "r")
